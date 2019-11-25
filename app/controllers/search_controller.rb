dedebcacdb8e25cb047aef5eb06a8f8e719a39c2
Michael Kofron <kofronmichael@gmail.com>
2019-11-25 02:49:53 -0600

    hello

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,6 @@ class SearchController < ApplicationController
         if logged_in?
             @user = User.find_by_id(session[:user_id])
             erb :searchpage
-            Quotes.clear_all
         else
             redirect '/'
         end
